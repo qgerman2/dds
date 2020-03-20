a2c7e6b185d7311da7a7624cbccb696313d67e54
qgerman2 <qgerman2@gmail.com>
2020-03-20 03:05:08 -0300

    arm7

--- include/render.h
+++
@@ -1,9 +0,0 @@
-#ifndef RENDER
-#define RENDER
-extern bool sprites[128];
-extern bool spritesSub[128];
-u8 popSprite();
-u8 popSpriteSub();
-void pushSprite(u8 i);
-void pushSpriteSub(u8 i);
-#endif
--- include/sound.h
+++
@@ -1,1 +0,0 @@
-void s_play();
