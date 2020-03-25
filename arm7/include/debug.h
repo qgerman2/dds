8c59ae91361aa66967a31c2aeedb508fc7b72218
qgerman2 <qgerman2@gmail.com>
2020-03-25 20:46:59 -0300

    sin arm7

--- arm7/include/debug.h
+++
@@ -1,4 +0,0 @@
-#ifndef DEBUG_ARM7
-#define DEBUG_ARM7
-void debugMessage(const char* msg);
-#endif 
--- arm9/include/sound.h
+++
@@ -1,7 +0,0 @@
-#ifndef SOUND
-#define SOUND
-void s_play();
-void loadogg();
-void loadmp3();
-void fillmp3();
-#endif
