ccf32a09caa4432cb4f95e4fa00b32f71cf0117b
qgerman2 <qgerman2@gmail.com>
2020-04-08 00:39:42 -0400

    mmm

--- include/main.h
+++ include/main.h
@@ -6,11 +6,9 @@
 void imagetobg(std::string path);
 void testpng();
 void testjpeg();
-extern songdata song;
 extern int state;
 extern std::string simpath;
 extern std::string songpath;
 extern int bgid;
-extern songdata song;
 extern Buffer* shared_buffer;
 #endif
