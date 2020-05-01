fc2deac25fdce5d85e65bf5ac5a409f05001a9fb
qgerman2 <qgerman2@gmail.com>
2020-05-01 00:31:00 -0400

    mines setting works

--- source/play.cpp
+++ source/play.cpp
@@ -363,7 +363,9 @@ void Play::newSteps(u16 data, u32 beatf, u8 notetype) {
 		}
 		if (data & mine[i]) {
 			s.type = 6;
-			newstep = true;
+			if (settings.mines) {
+				newstep = true;
+			}
 		}
 		if (newstep) {
 			s.x = (HITXOFFSET + 32 * i);
